819175d7dedfde602410990bd984f40187d8f69d
evgenyKravchenko <evgenyonechenko@gmail.com>
2025-07-01 10:01:33 +0600

    feat(auth): добавить API-роут для регистрации пользователя - Реализована POST-обработка данных формы (email, имя, пароль). - Пароль хэшируется с помощью bcrypt перед сохранением. - Возвращается созданный пользователь или соответствующая ошибка.

---
+++ app/api/register/route.ts
@@ -0,0 +1,46 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismaDB";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const {
+            email,
+            name,
+            password
+        } = body;
+
+        if (!email || !name || !password) {
+            return new Response(
+                JSON.stringify({ error: "Missing info" }),
+                {
+                    status: 400,
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prisma.user.create({
+            data: {
+                email: email,
+                name: name,
+                hashedPassword: hashedPassword
+            }
+        });
+
+        return NextResponse.json(user);
+    } catch (error: any) {
+        console.log(error, 'REGISTRATION_ERROR');
+        return new Response(
+            JSON.stringify({ error: "Internal error" }),
+            {
+                status: 500,
+                headers: { "Content-Type": "application/json" },
+            }
+        );
+    }
+}
